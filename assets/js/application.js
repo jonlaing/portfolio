4d2f3f2a4ac7d1ff97727240013d319ae63a35ea
jonlaing <jon.dreaux.laing@gmail.com>
2012-04-19 18:19:25 -0400

    fix cycling problem when modal is open

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,6 +2,13 @@ jQuery(function($) {
         $('.carousel .item').first().addClass('active');
 	$('.dropdown-toggle').dropdown();
         $('.carousel').carousel();
+
+  $('.modal').bind('show', function () {
+    $('.carousel').carousel('pause');
+  }).bind('hide', function () {
+    $('.carousel').carousel('cycle');
+  });
+
 	$('.carousel').bind('slide', function () {
 		resize_image_info();
 	});
